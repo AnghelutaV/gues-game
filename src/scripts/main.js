822e076ff49b2bbf58bdd5c17f2af8369561ad2e
Veaceslav Barbarii <slava.barbarii@gmail.com>
2023-03-03 16:58:31 +0200

    Patched JS code for main index.html page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,36 +1,61 @@
 
-let goButton = document.querySelector("#goButton");
-let resetButton = document.querySelector("#resetButton");
+const goButton = document.querySelector("#goButton");
+const resetButton = document.querySelector("#resetButton");
+const errorMessageNode = document.querySelector("#long");
 
 goButton.addEventListener("click", goButtonClickHandler);
 resetButton.addEventListener("click", resetButtonClickHandler);
 
+let wordIndex = 0;
 
 function goButtonClickHandler() {
-	let count = 1;
-	let x = document.querySelector("#ph").value;
-	
-	if (x.length == 6) {
 
-		for (var i = 0; i < x.length; i++) {
+	const inputText = document.querySelector("#ph").value;
+	const wordLength = inputText.length;
 
-				var idWord= '#count+"-"+i';
-			document.querySelector(idWord).innerHTML = x[i];
-		}
-		
-		count++;
-		
-	}else if (x.length > 6) 
-	{
-		document.querySelector("#long").innerHTML = 'Atenție! Ați introdus un cuvînt prea lung!';
-		
-	}else (x.length < 6) 
-	{
-		document.querySelector("#long").innerHTML = 'Atenție! Ați introdus un cuvînt prea scurt!';
-	};
+	if (wordLength > 6) {
+		errorMessageNode.innerHTML = 'Atenție! Ați introdus un cuvînt prea lung!';
+		return;
+	}
+
+	if (wordLength < 6) {
+		errorMessageNode.innerHTML = 'Atenție! Ați introdus un cuvînt prea scurt!';
+		return;
+	}
+
+	if (wordIndex >= 6) {
+		errorMessageNode.innerHTML = 'Atenție! Tabelul este plin!';
+		return;
+	}
+
+	const wordRow = document.querySelector(`#tab > tbody > tr:nth-child(${++wordIndex})`);
+	const letters = inputText.split('');
+
+	const letterCells = [...wordRow.childNodes.values()]
+		.filter((node) => node.nodeName === 'TD' && node.classList.contains('letter'));
+
+	letters.forEach((letter, index) => {
+		letterCells[index].innerHTML = letter;
+	});
 };
 
-function resetButtonClickHandler()
-{
+function resetButtonClickHandler() {
+	location.reload();
+};
+
+// activated on page index.html
+const stubPageClickHandler = e => {
+	e.preventDefault();
+	window.history.replaceState({}, "", "/");
+	alert("This page isn't done yet!");
+};
+
+const page2 = document.querySelector("#page2");
+const page3 = document.querySelector("#page3");
+
+page2.addEventListener("click", stubPageClickHandler);
+page3.addEventListener("click", stubPageClickHandler);
+
+function resetButtonClickHandler() {
 	location.reload();
 };
