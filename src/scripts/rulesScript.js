007be5f8c8bdb80c50a1362d847dabfa2071a64f
Veaceslav Barbarii <slava.barbarii@gmail.com>
2023-03-03 16:57:47 +0200

    Patched JS code for rules.html page

--- src/scripts/rulesScript.js
+++ src/scripts/rulesScript.js
@@ -1,18 +1,18 @@
 
 function myFunction() { // give it a proper name
-	let text = document.querySelector("#iname").value;
+	const text = document.querySelector("#iname").value;
 
 	document.querySelector("#demo").innerHTML = text + " - You wrote ";
 };
 
 function signerIn() {
-	let name = document.querySelector("#iname").value;
-	let parola = document.querySelector("#ipassword").value;
+	const name = document.querySelector("#iname").value;
+	const parola = document.querySelector("#ipassword").value;
 
-	if (name == "Victor" && parola == "0909") {
+	if (name === "Victor" && parola === "0909") {
 		document.querySelector("#demo1").innerHTML = "Good to see you " + name;
 		return;
-	} 
+	}
 
 	document.querySelector("#demo1").innerHTML = "Sorry, this Acount doesn't exist!";
 };
@@ -27,23 +27,20 @@ alertB.addEventListener("click", functionAlert);
 function functionAlert() {
 	alert("hello alert system");
 };
- 
-// this event doesn't work 
 
-var page2 = document.querySelector("#page2");
-
-page2.onclick = function () 
-{
-	alert("this page is not done yet!");
+// activated on page rules.html
+const stubPageClickHandler = e => {
+	e.preventDefault();
+	window.history.replaceState({}, "", "/");
+	alert("This page isn't done yet!");
 };
 
-var page3 = document.querySelector("#page3");
+const page2 = document.querySelector("#page2");
+const page3 = document.querySelector("#page3");
 
-page3.onclick = function () {
-	alert("this page is not done yet!");
-};
+page2.addEventListener("click", stubPageClickHandler);
+page3.addEventListener("click", stubPageClickHandler);
 
-function resetButtonClickHandler()
-{
+function resetButtonClickHandler() {
 	location.reload();
 };
